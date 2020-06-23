6ea9d146f08a5c1c5bb4fc848905adde7d8c5b58
Kai Haskell <me@kaihaskell.com>
2020-06-22 22:03:14 -0400

    Fixed something on a second go-around

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import NasaAotd from "./components/nasaCardInfo.js";
 import NavBar from "./components/navBar";
 import "./App.css";
